0071f15d1b48cb0abe38a89856369181ee0b9fac
Ivaan3 <106582115+Ivaan3@users.noreply.github.com>
2022-12-20 19:33:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const nombre = "Ivan"
+const apellido = "Alvaretto"
+
+const persona = {
+    nombre: nombre,
+    apellido: apellido,
+}
+
+//sessionStorage.setItem("persona", JSON.stringify(persona))
+
+//localStorage.setItem("persona", JSON.stringify(persona))
+
+//document.cookie = "caducar= Ivan Alvaretto; expires" + new Date(2022, 11, 20, 0, 0, 2).toUTCString()
+//localStorage.removeItem("")
